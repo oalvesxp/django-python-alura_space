c361c34891952b1f41c02340deb1b9949f44b496
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-14 15:41:27 -0300

    changing function for just name

--- alura_space/galeria/models.py
+++ alura_space/galeria/models.py
@@ -18,4 +18,4 @@ class Fotografia(models.Model):
     data = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
-        return f"Fotografia [nome={self.nome}]"
+        return self.nome
