55ff1d81ff6d023b689fd842ba114244890abd40
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-14 13:48:58 -0300

    adding a new field 'CATEGORIA' on object 'Fotografia' and making migration

--- alura_space/galeria/models.py
+++ alura_space/galeria/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 class Fotografia(models.Model):
+    CATEGORIAS = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALAXIA", "Galaxia"),
+        ("PLANETAS", "Planeta"),
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=CATEGORIAS, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
