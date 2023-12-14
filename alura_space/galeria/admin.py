2bd6d4744b57c1a71ebb5cc784328f43e9fe407a
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-14 13:34:29 -0300

    setuping display list for 'Fotografia' in admin console

--- alura_space/galeria/admin.py
+++ alura_space/galeria/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from galeria.models import Fotografia
 
-admin.site.register(Fotografia)
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+
+admin.site.register(Fotografia, ListandoFotografias)
