778cb26dcfd9f0b6fd1061549b2b6c4506a1eb6e
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-14 13:41:41 -0300

    adding search fileds on admin console

--- alura_space/galeria/admin.py
+++ alura_space/galeria/admin.py
@@ -4,5 +4,6 @@ from galeria.models import Fotografia
 class ListandoFotografias(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda")
     list_display_links = ("id", "nome")
+    search_fields = ("nome",)
 
 admin.site.register(Fotografia, ListandoFotografias)
