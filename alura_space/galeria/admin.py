c160596c583bcfe5fe466938d45cb00ef5a25df9
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-14 14:04:58 -0300

    adding new field on list display

--- alura_space/galeria/admin.py
+++ alura_space/galeria/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "publicada")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
     list_filter = ("categoria",)
