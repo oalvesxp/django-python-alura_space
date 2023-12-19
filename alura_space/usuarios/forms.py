3c2da82846f2d6421858219b5543410dbc397144
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-19 13:44:11 -0300

    formating the form in login page

--- alura_space/usuarios/forms.py
+++ alura_space/usuarios/forms.py
@@ -4,11 +4,22 @@ class LoginForms(forms.Form):
     nome_login=forms.CharField(
         label='Nome de Login',
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'example@mail.com'
+            }
+        )
     )
     senha=forms.CharField(
         label='Senha',
         required=True,
         max_length=70,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha...'
+            }
+        )
     )
