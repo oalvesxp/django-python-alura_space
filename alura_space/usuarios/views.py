de9b81f3b37d2f9ca765ac4bf48cb19a67953a32
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-19 14:29:42 -0300

    setuping logical for form cadastro

--- alura_space/usuarios/views.py
+++ alura_space/usuarios/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
+from django.contrib.auth.models import User
+
 
 def login(request):
     form = LoginForms()
@@ -7,4 +9,28 @@ def login(request):
 
 def cadastro(request):
     form = CadastroForms()
+
+    if request.method == 'POST':
+        form == CadastroForms(request.POST)
+        
+        if form.is_valid():
+            if form['senha_1'].value() != form['senha_2'].value():
+                return redirect('cadastro')
+
+            nome=form['nome_cadastro'].value()
+            email=form['email'].value()
+            senha=form['senha_1'].value()
+
+            if User.objects.filter(email=nome).exists():
+                return redirect('cadastro')
+
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            
+            usuario.save()
+            return redirect('login')
+
     return render(request, 'usuarios/cadastro.html', {'form': form})
