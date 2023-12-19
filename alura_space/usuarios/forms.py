6efb1535628d1aedef7e836aaf3c70c3bbc00409
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-19 15:18:15 -0300

    updating LoginForms

--- alura_space/usuarios/forms.py
+++ alura_space/usuarios/forms.py
@@ -2,13 +2,13 @@ from django import forms
 
 class LoginForms(forms.Form):
     nome_login=forms.CharField(
-        label='Nome de Login',
+        label='Usuário',
         required=True,
         max_length=100,
         widget=forms.TextInput(
             attrs={
                 'class': 'form-control',
-                'placeholder': 'example@mail.com'
+                'placeholder': 'Digite seu usuário...'
             }
         )
     )
