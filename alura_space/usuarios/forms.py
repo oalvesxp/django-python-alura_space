17d9671b3342d187c6cb8c70bcbacab616ecf611
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-19 14:05:56 -0300

    creating object for forms 'cadastro' and import this form in html page

--- alura_space/usuarios/forms.py
+++ alura_space/usuarios/forms.py
@@ -23,3 +23,49 @@ class LoginForms(forms.Form):
             }
         )
     )
+
+class CadastroForms(forms.Form):
+    nome_cadastro=forms.CharField(
+        label='Nome de Login',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Nome completo...'
+            }
+        )
+    )
+    email=forms.EmailField(
+        label='E-mail',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'example@mail.com'
+            }
+        )
+    )
+    senha_1 =forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha...'
+            }
+        )
+    )
+    senha_2 =forms.CharField(
+        label='Confirmação de senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha novamente...'
+            }
+        )
+    )
