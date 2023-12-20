f9970d86dcb2122aaa504be29eabc85a568bb56d
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-20 18:59:45 -0300

    indexing the image to the user and inserting a validation in the registration

--- alura_space/usuarios/forms.py
+++ alura_space/usuarios/forms.py
@@ -69,3 +69,13 @@ class CadastroForms(forms.Form):
             }
         )
     )
+
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get("nome_cadastro")
+
+        if nome:
+            nome = nome.strip()
+            if " " in nome:
+                raise forms.ValidationError("Não pode conter espaços no campo de 'Nome de Cadastro'!")
+            else:
+                return nome
